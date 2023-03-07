650a3c811b6278ae869e18a12363d2ac4c45615f
Mariia <shvetsmariia@yahoo.com>
2023-03-06 18:03:32 -0600

    updated validation functions

--- validationLibrary.js
+++ validationLibrary.js
@@ -2,11 +2,9 @@ function validateName (inputName) {
     const nameRegex = /^[a-z ,.'-]+$/i; 
     
     if(inputName.value.match(nameRegex)) {
-        // alert("");
         return true;
     } else
     {
-        alert("Enter a valid name");
         return false;
     }
 }
@@ -15,11 +13,9 @@ function validatePhone (inputPhone) {
     const phoneRegex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
     
     if(inputPhone.value.match(phoneRegex)) {
-        // alert("You entered a valid phone number");
         return true;
     } else
     {
-        alert("Please enter a valid phone number");
         return false;
     }
 }
@@ -28,11 +24,9 @@ function validateEmail (inputEmail) {
     const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     
     if(inputEmail.value.match(emailRegex)) {
-        // alert("You entered a valid email address");
         return true;
     } else
     {
-        alert("Please enter a valid email address");
         return false;
     }
 }
@@ -41,11 +35,9 @@ function validatePassword (inputPassword) {
     const passwordRegex = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/; 
     
     if(inputPassword.value.match(passwordRegex)) {
-        // alert("You entered a valid password");
         return true;
     } else
     {
-        alert("Please enter a valid password");
         return false;
     }
 }
